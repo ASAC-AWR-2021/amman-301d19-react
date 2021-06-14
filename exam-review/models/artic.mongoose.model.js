29d2d4293bc0778c6756556acaeb36257dcf8242
AbeerAl-Rafati <Abeerw.rafati@yahoo.com>
2021-06-15 01:53:40 +0300

    archive

---
+++ exam-review/models/artic.mongoose.model.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+
+// Create a schema for our model
+const artPieceSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    slug: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    thumbnail: String,
+    artist_name: String,
+    description: String
+});
+
+
+// model our schema
+const artPieceModel = mongoose.model('art_pieces', artPieceSchema)
+// export the model to be used
+module.exports = artPieceModel;
